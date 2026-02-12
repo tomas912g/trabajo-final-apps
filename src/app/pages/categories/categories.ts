9774e71ac6adceaf5e6de264fa72c11bd0d4a58d
matepeira <mateopeirano21@gmail.com>
2026-02-12 15:36:25 -0300

    comentarios categories y restaurant-list

--- src/app/pages/categories/categories.ts
+++ src/app/pages/categories/categories.ts
@@ -11,45 +11,45 @@ import { Category, CategoryId } from '../../interfaces/category';
   styleUrl: './categories.scss',
 })
 export class CategoriesFormComponent implements OnInit {
-  private categoriesService = inject(CategoriesService);
-  categoryIdToEdit = input<number | null>(null);
+  private categoriesService = inject(CategoriesService); // injectamos dependencia
+  categoryIdToEdit = input<number | null>(null); // usamos la funcion input en vez del @Input(), lo que hace que la variable sea reactiva. puede recibir un numero o null
 
-  categoryData: Category = {
+  categoryData: Category = { // Hacemos el modelo de formulario que se conecta con los inputs a traves de NGModel
     name: '',
     description: ''
   };
-  isEditMode = false;
+  isEditMode = false; // lo usamos para saber en que modo estamos
 
   async ngOnInit(): Promise<void> {
-    const id = this.categoryIdToEdit();
-    if (id) { 
-    this.isEditMode = true;
+    const id = this.categoryIdToEdit(); // leemos el valor del signal ejecutandolo como funcion para saber el modo en el que estamos
+    if (id) { // si hay id
+    this.isEditMode = true; // estamos en modo edicion
     try { 
-      const data = await this.categoriesService.getCategoryById(id);
+      const data = await this.categoriesService.getCategoryById(id); // pedimos al backend los datos de la categoria que estamos editando para mostrarlos en los input
 
-      this.categoryData = {
+      this.categoryData = { // Rellenamos los datos del modelo de formulario
         name: data.name,
         description: data.description
       };
-    } catch (error) {
+    } catch (error) { // manejamos error en caso de que haya
       console.error('Error al cargar categoría:', error);
     }
   }
 }  
 
-  async onSubmit() {
+  async onSubmit() { // envio del forulario
     try {
       const id = this.categoryIdToEdit();
-      if (this.isEditMode && id !== null) {
-        // actualiza categoria
+      if (this.isEditMode && id !== null) { //(PUT)
+        // actualiza categoria, usamos el id para saber cual modificar y mandamos los nuevos datos
         await this.categoriesService.updateCategory(id, this.categoryData); 
-        alert('Categoria actualizada');
-      } else {
-        //crea categoria
+        alert('Categoria actualizada'); // damos un feedback simple
+      } else { // (POST)
+        //crea categoria si no hay un id ya creado para la que estamos buscando
         await this.categoriesService.createCategory(this.categoryData);
-        alert('Categoria creada');
+        alert('Categoria creada'); // respuesta simple
       }
-      } catch (error) {
+      } catch (error) { // maneja erro
       alert('Error al guardar la categoría.');
     }
   }
--- src/app/pages/restaurant-list/restaurant-list.ts
+++ src/app/pages/restaurant-list/restaurant-list.ts
@@ -10,33 +10,31 @@ import { CommonModule } from '@angular/common';
   templateUrl: './restaurant-list.html',
   styleUrl: './restaurant-list.scss',
 })
-export class RestaurantList implements OnInit{
+export class RestaurantList implements OnInit{ // usamos onInit para ejecutar el codigo al arrancar
   restaurantService = inject(RestaurantService)
-  restaurants: Restaurant[] = []; 
-  isLoading: boolean = true;
+  restaurants: Restaurant[] = []; // creamos un array vacio de restaurants donde guardaremos la lista que llegue del servidor
+  isLoading: boolean = true; // cuando funciona o no el spinner, comienza en true
 
-  async ngOnInit() {
-    this.isLoading = true;
+  async ngOnInit() { // esta funcion se ejecuta automaticamente apenas el componente aparece en pantalla
+    this.isLoading = true; // mostramos spinner
     try{
-    const data = await this.restaurantService.getRestaurants();
-    console.log("Datos recibidos del backend:", data); 
-    this.restaurants = data;
-      this.restaurants = await this.restaurantService.getRestaurants();
-      } catch (error){
+    const data = await this.restaurantService.getRestaurants(); // peticion de datos y espera (await) a que el servidor traiga el JSON del backend
+    this.restaurants = data; // guardamos los datos recibidos en nuestra variable
+      } catch (error){ // si el servidor o internet fallan, mostramos el error en la consola
       console.error(error);
-        } finally{
+        } finally{ // pase lo que pase frenamos el spinner
           this.isLoading = false;
         }
   }
 
   async onToggleFavorite(restaurant: Restaurant) {
-    const originalStatus = restaurant.isFavorite;
-    restaurant.isFavorite = !restaurant.isFavorite;
+    const originalStatus = restaurant.isFavorite; // guardamos el estado original del restaurante por si la peticion falla
+    restaurant.isFavorite = !restaurant.isFavorite; // hacemos el cambio visual automatico antes de hacer la peticion al servidor para que el cambio sea mas rapido
   try {
-    await this.restaurantService.toggleRestaurantFavorite(restaurant.id);
+    await this.restaurantService.toggleRestaurantFavorite(restaurant.id); // llamamos a la funcion  para hacer el cambio y que el servidor lo confirme
 
-    } catch (error) {
-      restaurant.isFavorite = originalStatus;
+    } catch (error) { // si hay error
+      restaurant.isFavorite = originalStatus; // devolvemos el corazon a su estado original (se despinta) para que el usuario sepa que no funciono
       console.error("Error al seleccionar restaurante:", error);
     }
 }
